78c32cb991ba290e5eab6b084f218084f73910d4
Chris <moorecash@gmail.com>
2023-03-25 14:08:12 -0400

    additional tests

--- tests/netflix.spec.js
+++ tests/netflix.spec.js
@@ -17,7 +17,7 @@ test("my first test", async ({ page }) => {
 });
 
 // ADD YOUR TESTS HERE!
-// green 
+// gree
 test.use({ headless: false, slowMo: 1000 });
 
 test("homework", async ({ page }) => {
